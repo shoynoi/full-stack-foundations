240d42e10b64ed7e42e59eb4aff45244981049bb
shoynoi <shoynoi.jp@gmail.com>
2023-10-01 22:28:45 +0900

    02-02 Links
    - Remix の Link は react-router を元に実装されている
    - Link の `..` はデフォルトではRouterの階層を一つ上がる挙動をするが、 relative="path" を指定することでURLセグメントを一つ上がるように振る舞いを変えることができる
      - Route がネストしていない時に一つ上の Path に戻りたい場合に利用できる

--- exercises/02.routing/02.problem.links/app/routes/users+/kody_+/notes.tsx
+++ exercises/02.routing/02.problem.links/app/routes/users+/kody_+/notes.tsx
@@ -1,14 +1,23 @@
-import { Outlet } from '@remix-run/react'
+import { Link, Outlet } from '@remix-run/react'
 
 export default function NotesRoute() {
 	return (
 		<div className="flex h-full justify-between pb-12 border-8 border-blue-500">
-			<h1 className="text-h1">Notes</h1>
-			{/* 🐨 add two links here.
-				One to go back to the parent *path* (💰 not parent "route" that's why you need to use the relative="path" prop)
-				and the other to go to the some-note-id route
-			*/}
-			{/* 💰 feel free to restructure things however you like to make them look nice */}
+			<div>
+				<h1 className="text-h1">Notes</h1>
+				<ul>
+					<li>
+						<Link to=".." relative="path" className="underline">
+							Back to Kody
+						</Link>
+					</li>
+					<li>
+						<Link to="some-note-id" className="underline">
+							Some Note
+						</Link>
+					</li>
+				</ul>
+			</div>
 			<Outlet />
 		</div>
 	)

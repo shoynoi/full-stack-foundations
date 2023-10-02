08118e74fb91285bc07ae65c4ab3619e21d67565
shoynoi <shoynoi.jp@gmail.com>
2023-10-02 22:19:03 +0900

    03-01 Loaders
    - loader 関数で Request オブジェクトを受け取り、 Response オブジェクトを返すことができる
    - `json` を使うと容易にJSON形式のResponseオブジェクトを返すことができる
    - 返された Response データは useLoaderData から取得できる

--- exercises/03.loading/01.problem.loader/app/routes/users+/$username_+/notes.tsx
+++ exercises/03.loading/01.problem.loader/app/routes/users+/$username_+/notes.tsx
@@ -1,39 +1,34 @@
-import { Link, NavLink, Outlet, useParams } from '@remix-run/react'
+import { type DataFunctionArgs, json } from '@remix-run/node'
+import { Link, NavLink, Outlet, useLoaderData } from '@remix-run/react'
+import { db } from '#app/utils/db.server.ts'
 import { cn } from '#app/utils/misc.ts'
-// 🐨 get the db utility using:
-// import { db } from '#app/utils/db.server.ts'
 
-// 🐨 add a `loader` export here which uses the params from the DataFunctionArgs
-// 🐨 you'll get the username from params.username
-// 💰 Here's how you get the owner information and the note from the database:
-// const owner = db.user.findFirst({
-// 	where: {
-// 		username: { equals: username, },
-// 	},
-// })
-// const notes = db.note
-// 	.findMany({
-// 		where: {
-// 			owner: {
-// 				username: { equals: username, },
-// 			},
-// 		},
-// 	})
-// 🐨 return the necessary data using Remix's json util
-// 🦺 TypeScript will complain about the owner being possibly undefined, we'll
-// fix that in the next section
-// 💯 as extra credit, try to do it with new Response instead of using the json util just for fun
-// 🦉 Note, you should definitely use the json helper as it's easier and works better with TypeScript
-// but feel free to try it with new Response if you want to see how it works.
+export async function loader({ params: { username } }: DataFunctionArgs) {
+	const owner = db.user.findFirst({
+		where: {
+			username: { equals: username },
+		},
+	})
+	const notes = db.note
+		.findMany({
+			where: {
+				owner: {
+					username: { equals: username },
+				},
+			},
+		})
+		.map(({ id, title }) => ({
+			id,
+			title,
+		}))
+	return json({ owner, notes })
+}
 
 export default function NotesRoute() {
-	// 💣 we no longer need the params, delete this
-	const params = useParams()
-	// 🐨 get the data from useLoaderData
-	// 🐨 update the ownerDisplayName to be what you get from the loader data
-	// 💯 note, the user's name is not required, so as extra credit, add a
-	// fallback to the username
-	const ownerDisplayName = params.username
+	const data = useLoaderData<typeof loader>()
+
+	// @ts-expect-error
+	const ownerDisplayName = data.owner.name ?? data.owner.username
 	const navLinkDefaultClassName =
 		'line-clamp-2 block rounded-l-full py-2 pl-8 pr-6 text-base lg:text-xl'
 	return (
@@ -42,8 +37,8 @@ export default function NotesRoute() {
 				<div className="relative col-span-1">
 					<div className="absolute inset-0 flex flex-col">
 						<Link
-							// 🐨 we can get the username from the loader data instead
-							to={`/users/${params.username}`}
+							// @ts-expect-error
+							to={`/users/${data.owner.username}`}
 							className="pb-4 pl-8 pr-4 pt-12"
 						>
 							<h1 className="text-base font-bold md:text-lg lg:text-left lg:text-2xl">
@@ -51,20 +46,18 @@ export default function NotesRoute() {
 							</h1>
 						</Link>
 						<ul className="overflow-y-auto overflow-x-hidden pb-12">
-							{/*
-							🐨 instead of hard coding the note, create one <li> for each note
-							in the database with data.notes.map
-						*/}
-							<li className="p-1 pr-0">
-								<NavLink
-									to="some-note-id"
-									className={({ isActive }) =>
-										cn(navLinkDefaultClassName, isActive && 'bg-accent')
-									}
-								>
-									Some Note
-								</NavLink>
-							</li>
+							{data.notes.map(note => (
+								<li className="p-1 pr-0">
+									<NavLink
+										to={note.id}
+										className={({ isActive }) =>
+											cn(navLinkDefaultClassName, isActive && 'bg-accent')
+										}
+									>
+										{note.title}
+									</NavLink>
+								</li>
+							))}
 						</ul>
 					</div>
 				</div>
